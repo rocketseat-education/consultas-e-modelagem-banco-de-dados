6cda2db3a428d794bbe9f5e445ec5714c8277429
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 20:27:19 -0300

    Uso do Coalesce

--- comandos_2.sql
+++ comandos_2.sql
@@ -43,3 +43,7 @@ SELECT c.customer_id, c.first_name, c.last_name, o.order_id FROM orders AS o RIG
 SELECT p.product_id, p.product_name, c.category_id, c.category_name FROM products AS p FULL JOIN categories AS c ON p.category_id = c.category_id ORDER BY c.category_id, p.product_id;
 
 SELECT o.order_id, o.customer_id AS pedido_customer_id, o.order_date, o.status, c.customer_id AS cliente_customer_id, c.first_name || ' ' || c.last_name AS cliente, c.city FROM orders AS o FULL JOIN customers AS c ON o.customer_id = c.customer_id ORDER BY o.order_date DESC;
+
+SELECT COALESCE(c.first_name, 'SEM NOME') AS primeiro_nome, COALESCE(c.last_name, 'DESCONHECIDO') AS sobrenome FROM customers AS c;
+
+SELECT c.customer_id, COALESCE(c.first_name, 'SEM NOME') AS primeiro_nome, COALESCE(c.last_name, 'DESCONHECIDO') AS sobrenome, COALESCE(o.total_amount, 0) AS total_ultimo_pedido FROM customers AS c LEFT JOIN (SELECT customer_id, SUM(total_amount) AS total_amount FROM orders WHERE order_date >= '2024-01-01' GROUP BY customer_id) AS o ON c.customer_id = o.customer_id;
