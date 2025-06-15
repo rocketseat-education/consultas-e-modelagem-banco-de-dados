1db3acd2ad2d49b75421c5de9bc59b90449fecd4
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 17:43:12 -0300

    Uso Order By

--- comandos.sql
+++ comandos.sql
@@ -75,3 +75,17 @@ SELECT * FROM products ORDER BY price ASC;
 SELECT MIN(created_at) AS primeira_data_cadastro FROM customers;
 
 SELECT MAX(created_at) AS ultima_data_cadastro FROM customers;
+
+SELECT product_id, product_name, price FROM products ORDER BY price ASC;
+
+SELECT product_id, product_name, price FROM products ORDER BY price DESC;
+
+SELECT * FROM customers ORDER BY first_name ASC;
+
+SELECT * FROM customers ORDER BY first_name DESC;
+
+SELECT * FROM customers ORDER BY created_at ASC;
+
+SELECT * FROM customers ORDER BY created_at DESC;
+
+SELECT order_id, status, order_date FROM orders ORDER BY status ASC, order_date DESC;
