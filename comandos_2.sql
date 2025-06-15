b366cc086bb7bfce2617ff1fb24577c2fdd8c983
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 19:34:46 -0300

    Uso do Inner Join

--- comandos_2.sql
+++ comandos_2.sql
@@ -11,3 +11,7 @@ SELECT p.product_id, p.product_name FROM products p WHERE NOT EXISTS (SELECT 1 F
 SELECT o.status, SUM(CASE WHEN o.status = 'DELIVERED' THEN o.total_amount ELSE 0 END) AS total_entregue, SUM(CASE WHEN o.status = 'PENDING' THEN o.total_amount ELSE 0 END) AS total_pendente, SUM(o.total_amount) AS total_geral FROM orders o GROUP BY o.status;
 
 SELECT DATE_TRUNC('month', o.order_date) AS mes, SUM(o.total_amount) AS total_geral, SUM(o.total_amount) FILTER (WHERE o.status = 'DELIVERED') AS total_entregue, SUM(o.total_amount) FILTER (WHERE o.status = 'PENDING') AS total_pendente FROM orders o GROUP BY DATE_TRUNC('month', o.order_date) ORDER BY mes;
+
+SELECT p.product_id, p.product_name, c.category_id, c.category_name FROM products AS p INNER JOIN categories AS c ON p.category_id = c.category_id ORDER BY p.product_id;
+
+SELECT p.product_id AS pid, p.product_name AS nome_produto, c.category_id, c.category_name FROM products AS p INNER JOIN categories AS c ON p.category_id = c.category_id WHERE p.price > 100 ORDER BY pid;
