5e9193eb51af0262c8cd9b97cfd3b98322c154e5
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 17:10:06 -0300

    Uso IN e NOT IN com Where

--- comandos.sql
+++ comandos.sql
@@ -21,3 +21,15 @@ SELECT customer_id, first_name, last_name, city FROM customers WHERE city = 'Sao
 SELECT product_id, product_name, price FROM products WHERE price < 50 OR price > 400;
 
 SELECT order_id, total_amount, status FROM orders WHERE (status = 'DELIVERED' OR status = 'SHIPPED') AND total_amount > 300;
+
+SELECT order_id, status FROM orders WHERE status IN ('PENDING', 'SHIPPED');
+
+SELECT product_id, category_id FROM products WHERE category_id IN (1, 3, 5);
+
+SELECT customer_id FROM customers WHERE city = 'Manaus' OR city = 'Recife' OR city = 'Fortaleza';
+
+SELECT customer_id FROM customers WHERE city IN ('Manaus', 'Recife', 'Fortaleza');
+
+SELECT product_id, product_name, category_id FROM products WHERE category_id NOT IN (2, 4, 6);
+
+SELECT product_id, product_name FROM products WHERE category_id IS NOT NULL AND category_id NOT IN (2, 4, 6);
