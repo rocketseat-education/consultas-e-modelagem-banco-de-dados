a934e0c41a5af1fde9d2afee8279d9abc58133bf
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 20:40:10 -0300

    Junções em Três ou Mais Tabelas com Condição Adicional

--- comandos_2.sql
+++ comandos_2.sql
@@ -55,3 +55,5 @@ SELECT * FROM (SELECT p.product_id, p.product_name, oi.order_id, oi.quantity FRO
 SELECT c.customer_id, c.first_name, c.last_name, (SELECT COUNT (*) FROM orders o WHERE o.customer id = c.customer_id) AS total_pedidos FROM customers AS c;
 
 SELECT c.customer_id, c.first_name, COUNT(o.order_id) AS total_pedidos FROM customers AS c LEFT JOIN orders AS o ON c.customer_id = o.customer_id GROUP BY c.customer_id, c.first_name;
+
+SELECT cli.customer_id, CONCAT(cli.first_name, ' ', cli. last_name) AS nome_cliente, pd.order_id, TO_CHAR(pd.order_date, 'DD/MM/YYYY') AS data_pedido, pr.product_id, pr.product_name, ip.quantity, ip.unit_price, (ip.quantity * ip.unit_price) AS subtotal FROM customers AS cli INNER JOIN orders AS pd ON cli.customer_id = pd.customer_id INNER JOIN order_items AS ip ON pd.order_id = ip.order_id INNER JOIN products AS pr ON ip.product_id = pr.product_id WHERE pd.status = 'DELIVERED' ORDER BY cli.customer_id, pd.order_date;
