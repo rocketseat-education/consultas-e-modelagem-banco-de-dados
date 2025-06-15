292097b44672371cbe101b1ffadece2e231bec24
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 18:17:37 -0300

    Manipulação de Strings

--- comandos.sql
+++ comandos.sql
@@ -109,3 +109,17 @@ SELECT oi.product_id, SUM(oi.quantity) AS unidades_vendidas FROM order_items oi 
 SELECT oi.product_id, SUM(oi.quantity) AS qtd_total FROM order_items oi GROUP BY oi.product_id ORDER BY SUM(oi.quantity) DESC LIMIT 10;
 
 SELECT o.customer_id, SUM(o.total_amount) AS faturamento_total FROM orders o GROUP BY o.customer_id ORDER BY faturamento_total DESC LIMIT 5;
+
+SELECT customer_id, CONCAT(first_name, ' ', last_name) AS nome_concatenado1, first_name || ' ' || last_name AS nome_concatenado2 FROM customers LIMIT 5;
+
+SELECT customer_id, last_name, CHAR_LENGTH(last_name) AS tamanho_sobrenome FROM customers LIMIT 5;
+
+SELECT customer_id, UPPER(first_name) AS nome_maiusculo, LOWER(last_name) AS sobrenome_minusculo FROM customers LIMIT 5;
+
+SELECT ' Carlos ' AS texto_original, TRIM(' Carlos ') AS texto_trim, LTRIM(' Carlos ') AS texto_ltrim, RTRIM('Carlos  ') AS texto_rtrim;
+
+SELECT product_id, product_name, SUBSTRING(product_name FROM 1 FOR 3) AS primeiros_3_caracteres FROM products LIMIT 5;
+
+SELECT customer_id, last_name, POSITION('Silva' IN last_name) AS posicao_silva FROM customers WHERE last_name LIKE '%Silva%';
+
+SELECT customer_id, last_name, REPLACE(last_name, 'Silva', 'S.') AS sobrenome_abreviado FROM customers WHERE last_name LIKE '%Silva%';
