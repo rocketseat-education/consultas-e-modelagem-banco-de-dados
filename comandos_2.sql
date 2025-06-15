f6c0af5ca4de8315487ae02e998c9fa626c1ff8a
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 19:26:09 -0300

    Case e Filter

--- comandos_2.sql
+++ comandos_2.sql
@@ -8,3 +8,6 @@ SELECT * FROM orders o;
 
 SELECT p.product_id, p.product_name FROM products p WHERE NOT EXISTS (SELECT 1 FROM order_items oi WHERE oi.product_id = p.product_id) ORDER BY p.product_id;
 
+SELECT o.status, SUM(CASE WHEN o.status = 'DELIVERED' THEN o.total_amount ELSE 0 END) AS total_entregue, SUM(CASE WHEN o.status = 'PENDING' THEN o.total_amount ELSE 0 END) AS total_pendente, SUM(o.total_amount) AS total_geral FROM orders o GROUP BY o.status;
+
+SELECT DATE_TRUNC('month', o.order_date) AS mes, SUM(o.total_amount) AS total_geral, SUM(o.total_amount) FILTER (WHERE o.status = 'DELIVERED') AS total_entregue, SUM(o.total_amount) FILTER (WHERE o.status = 'PENDING') AS total_pendente FROM orders o GROUP BY DATE_TRUNC('month', o.order_date) ORDER BY mes;
