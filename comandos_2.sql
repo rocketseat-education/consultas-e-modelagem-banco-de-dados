289020349acc0be737a777e8428ab97d44212771
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 19:39:09 -0300

    Inner Join com Junção Múltipla

--- comandos_2.sql
+++ comandos_2.sql
@@ -15,3 +15,5 @@ SELECT DATE_TRUNC('month', o.order_date) AS mes, SUM(o.total_amount) AS total_ge
 SELECT p.product_id, p.product_name, c.category_id, c.category_name FROM products AS p INNER JOIN categories AS c ON p.category_id = c.category_id ORDER BY p.product_id;
 
 SELECT p.product_id AS pid, p.product_name AS nome_produto, c.category_id, c.category_name FROM products AS p INNER JOIN categories AS c ON p.category_id = c.category_id WHERE p.price > 100 ORDER BY pid;
+
+SELECT o.order_id, o.customer_id, c.first_name || ' ' || c.last_name AS cliente, o.order_date, o.status, o.total_amount FROM orders AS o INNER JOIN customers AS c ON o.customer_id = c.customer_id AND o.order_date >= c.created_at ORDER BY o.order_date DESC LIMIT 20;
