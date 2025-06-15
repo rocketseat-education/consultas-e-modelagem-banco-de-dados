a5c80f553cc6f15cd0587b3120a13a171b45812e
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 17:27:37 -0300

    Uso do Count e Distinct com Where

--- comandos.sql
+++ comandos.sql
@@ -45,3 +45,17 @@ SELECT customer_id, first_name, last_name FROM customers WHERE last_name LIKE '%
 SELECT customer_id, first_name, last_name FROM customers WHERE first_name LIKE '%ia%';
 
 SELECT product_id, product_name FROM products WHERE product_name LIKE '_roduto 1%';
+
+SELECT COUNT(*) AS total_pedidos FROM orders;
+
+SELECT COUNT(orders.total_amount) AS total_amount_nao_nulos FROM orders;
+
+SELECT COUNT(customers.last_name) AS total_customers_nao_nulos FROM customers;
+
+SELECT COUNT(DISTINCT customers.last_name) AS total_customers_nao_nulos FROM customers;
+
+SELECT DISTINCT customers.last_name FROM customers;
+
+SELECT COUNT(DISTINCT o.customer_id) AS clientes_unicos FROM orders o;
+
+SELECT COUNT(price) AS produtos_com_preco FROM products;
