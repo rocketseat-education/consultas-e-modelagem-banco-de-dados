0082c5cb10dcab65e9b20ab34cecde864fe7f77a
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 17:04:49 -0300

    Operadores AND e OR com Where

--- comandos.sql
+++ comandos.sql
@@ -15,3 +15,9 @@ INSERT INTO customers(customer_id, first_name, last_name, email, city, created_a
 SELECT * FROM customers WHERE last_name IS NULL;
 
 SELECT * FROM customers WHERE last_name IS NOT NULL;
+
+SELECT customer_id, first_name, last_name, city FROM customers WHERE city = 'Sao Paulo' AND created_at >= '2024-01-01';
+
+SELECT product_id, product_name, price FROM products WHERE price < 50 OR price > 400;
+
+SELECT order_id, total_amount, status FROM orders WHERE (status = 'DELIVERED' OR status = 'SHIPPED') AND total_amount > 300;
