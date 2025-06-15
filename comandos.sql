071a186ce841316be4a57f6c8dd144f694f14191
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-15 17:35:40 -0300

    Uso do SUM, AVG, MAX e MIN e Agrupamentos

--- comandos.sql
+++ comandos.sql
@@ -59,3 +59,19 @@ SELECT DISTINCT customers.last_name FROM customers;
 SELECT COUNT(DISTINCT o.customer_id) AS clientes_unicos FROM orders o;
 
 SELECT COUNT(price) AS produtos_com_preco FROM products;
+
+SELECT SUM(total_amount) AS faturamento_geral FROM orders;
+
+SELECT AVG(total_amount) AS media_pedidos FROM orders;
+
+SELECT COUNT(total_amount), SUM(total_amount) AS faturamento_geral, AVG(total_amount) AS media_pedidos FROM orders;
+
+SELECT AVG(price) AS media_precos FROM products;
+
+SELECT MAX(price) AS preco_maximo, MIN(price) AS preco_minimo FROM products;
+
+SELECT * FROM products ORDER BY price ASC;
+
+SELECT MIN(created_at) AS primeira_data_cadastro FROM customers;
+
+SELECT MAX(created_at) AS ultima_data_cadastro FROM customers;
