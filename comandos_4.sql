9280435fb17f770c7c2b57f199cb69e2fc1aa5f5
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-27 15:37:50 -0300

    Índices e Árvores B-Tree vs. Hash vs. GiST vs. GIN

--- comandos_4.sql
+++ comandos_4.sql
@@ -1,3 +1,11 @@
 EXPLAIN ANALYZE SELECT * FROM products WHERE price BETWEEN 100 AND 200;
 
 CREATE INDEX idx_products_price ON products(price);
+
+CREATE INDEX idx_products_price_hash ON products USING HASH(price); 
+
+EXPLAIN ANALYZE SELECT * FROM products WHERE price = 249.37;
+
+ALTER TABLE products ADD COLUMN tags TEXT[]; 
+
+UPDATE products SET tags = ARRAY['promo', 'novo'] WHERE product_id % 5 = 0;
