745e14673bac55d8f9a053d958ad2e8ecfec420d
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-27 15:42:57 -0300

    Escolha e criação de índices

--- comandos_4.sql
+++ comandos_4.sql
@@ -9,3 +9,11 @@ EXPLAIN ANALYZE SELECT * FROM products WHERE price = 249.37;
 ALTER TABLE products ADD COLUMN tags TEXT[]; 
 
 UPDATE products SET tags = ARRAY['promo', 'novo'] WHERE product_id % 5 = 0;
+
+CREATE INDEX idx_products_category ON products(category_id);
+
+CREATE UNIQUE INDEX idx_department_name_unq ON departments(department_name);
+
+CREATE INDEX idx_orders_pending ON orders(order_date) WHERE status = 'PENDING';
+
+CREATE INDEX idx_customers_lower_email ON customers (LOWER(email));
