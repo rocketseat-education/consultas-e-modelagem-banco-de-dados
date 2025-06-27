3c434d507b2622f612ecb1d6f61755657f5cb3b1
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-27 14:10:29 -0300

    Prática com Sub Consultas

--- comandos_3.sql
+++ comandos_3.sql
@@ -7,3 +7,7 @@ SELECT order_id, total_amount FROM orders WHERE total_amount = (SELECT MAX(total
 SELECT product_id, product_name FROM products p WHERE EXISTS (SELECT 1 FROM order_items oi WHERE oi.product_id = p.product_id);
 
 SELECT product_name, price, (SELECT AVG(price) FROM products p2 WHERE p2.category_id = p1.category_id) AS media_categoria FROM products p1;
+
+SELECT e.employee_id, e.first_name || ' ' || e.last_name AS nome, d.department_name, s.salary_amount FROM employees AS e INNER JOIN departments AS d ON e.department_id = d.department_id INNER JOIN salaries AS s ON e.employee_id = s.employee_id AND s.effective_to IS NULL WHERE s.salary_amount > (SELECT AVG(s2.salary_amount) FROM employees AS e2 INNER JOIN salaries AS s2 ON e2.employee_id = s2.employee_id AND s2.effective_to IS NULL WHERE e2.department_id = e.department_id ORDER BY d.department_name, s.salary_amount DESC;
+
+SELECT AVG(s.salary_amount) FROM employees AS e INNER JOIN departments AS d ON e.department_id = d.department_id INNER JOIN salaries AS s ON e.employee_id = s.employee_id AND s.effective_to IS NULL WHERE d.department_name = 'Marketing';
