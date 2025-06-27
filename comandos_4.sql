95e994defa85aa4c1eb0ed99c5c3406856f07595
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-27 15:56:13 -0300

    EXPLAIN - Leituras avançadas de planos

--- comandos_4.sql
+++ comandos_4.sql
@@ -33,3 +33,13 @@ EXPLAIN ANALYSE SELECT * FROM customers WHERE customer_id = 101;
 EXPLAIN SELECT * FROM products WHERE price > 300;
 
 EXPLAIN ANALYZE SELECT * FROM products WHERE price > 300;
+
+CREATE INDEX idx_price ON products(price);
+
+SET enable_seqscan = off;
+
+EXPLAIN ANALYZE SELECT * FROM products WHERE price > 300;
+
+SET enable_seqscan = on;
+
+EXPLAIN ANALYZE SELECT o.order_id, c.first_name, c.last_name FROM orders o JOIN customers c ON o.customer_id = c.customer_id WHERE o.status = 'SHIPPED';
