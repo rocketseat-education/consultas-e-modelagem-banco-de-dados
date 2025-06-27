ec8ce28a860e8f8b93e6832b1bb2da68594e1c2c
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-27 16:24:02 -0300

    Demais tipos de Particionamento de tabelas

--- comandos_4.sql
+++ comandos_4.sql
@@ -63,3 +63,37 @@ INSERT INTO orders_part (order_id, customer_id, order_date, status, total_amount
 SELECT * FROM orders_part WHERE order_date BETWEEN '2024-01-01' AND '2024-01-31';
 
 SELECT * FROM orders_part WHERE order_date BETWEEN '2024-02-01' AND '2024-02-28';
+
+CREATE TABLE orders_part (order_id INT GENERATED ALWAYS AS IDENTITY, customer_id INT, status VARCHAR (20), order_date DATE, total_amount NUMERIC(12,2)) PARTITION BY LIST (status);
+
+CREATE TABLE orders_shipped PARTITION OF orders_part FOR VALUES IN ('SHIPPED');
+
+CREATE TABLE orders_delivered PARTITION OF orders_part FOR VALUES IN ('DELIVERED');
+
+INSERT INTO orders_part (customer_id, status, order_date, total_amount) VALUES (123, 'SHIPPED', '2024-06-10', 150.00);
+
+CREATE TABLE customers_part (customer_id INT PRIMARY KEY, name TEXT) PARTITION BY HASH (customer_id);
+
+CREATE TABLE customers_part_0 PARTITION OF customers_part FOR VALUES WITH (MODULUS 4, REMAINDER 0);
+
+CREATE TABLE customers_part_1 PARTITION OF customers_part FOR VALUES WITH (MODULUS 4, REMAINDER 1);
+
+CREATE TABLE customers_part_2 PARTITION OF customers_part FOR VALUES WITH (MODULUS 4, REMAINDER 2);
+
+CREATE TABLE customers_part_3 PARTITION OF customers_part FOR VALUES WITH (MODULUS 4, REMAINDER 3);
+
+INSERT INTO customers_part (customer_id, name) VALUES (101, 'Alice');
+
+SELECT * FROM customers_part;
+
+CREATE TABLE orders_part (order_id INT GENERATED ALWAYS AS IDENTITY, status VARCHAR (20), customer_id INT) PARTITION BY LIST (status);
+
+CREATE TABLE orders_pending PARTITION OF orders_part FOR VALUES IN ('PENDING');
+
+CREATE TABLE orders_failed PARTITION OF orders_part FOR VALUES IN ('FAILED');
+
+CREATE TABLE orders_default PARTITION OF orders_part DEFAULT;
+
+INSERT INTO orders_part (status, customer_id) VALUES ('CANCELLED', 321);
+
+SELECT * FROM orders_part;
