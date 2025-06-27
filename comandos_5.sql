80b029ad8927fc12608fc16b7f9c1c33d318284a
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-27 17:09:37 -0300

    Introdução a Tabelas Temporárias

--- comandos_5.sql
+++ comandos_5.sql
@@ -27,3 +27,9 @@ CREATE INDEX idx_mv_vendas_mes ON mv_resumo_vendas_mensal(mes);
 SELECT * FROM mv_resumo_vendas_mensal WHERE mes BETWEEN '2024-01-01' AND '2024-06-01';
 
 CREATE MATERIALIZED VIEW mv_top_clientes_2024 AS SELECT c.customer_id, c.first_name || ' ' || c.last_name AS nome_completo, SUM(o.total_amount) AS total_gasto FROM customers c JOIN orders o ON c.customer_id = o.customer_id WHERE o.order_date BETWEEN '2024-01-01' AND '2024-12-31' GROUP BY c.customer_id, c.first_name, c.last_name HAVING SUM(o.total_amount) > 500;
+
+CREATE TEMPORARY TABLE tmp_pedidos_junho AS SELECT * FROM orders WHERE order_date BETWEEN '2024-06-01' AND '2024-06-30';
+
+CREATE TEMPORARY TABLE tmp_clientes_ativos AS SELECT DISTINCT c.customer_id, c.first_name || ' ' || c.last_name AS nome_completo FROM customers c JOIN orders o ON c.customer_id = o.customer_id WHERE o.order_date BETWEEN '2024-04-01' AND '2024-06-30';
+
+SELECT t.nome_completo, COUNT(o.order_id) AS total_pedidos FROM tmp_clientes_ativos t JOIN orders o ON t.customer_id = o.customer_id GROUP BY t.nome_completo ORDER BY total_pedidos DESC;
