3900679d81594b1150c12f5c12d930af346b0f07
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-27 14:22:08 -0300

    CTEs Não Recursivas e Recursivas

--- comandos_3.sql
+++ comandos_3.sql
@@ -13,3 +13,9 @@ SELECT e.employee_id, e.first_name || ' ' || e.last_name AS nome, d.department_n
 SELECT AVG(s.salary_amount) FROM employees AS e INNER JOIN departments AS d ON e.department_id = d.department_id INNER JOIN salaries AS s ON e.employee_id = s.employee_id AND s.effective_to IS NULL WHERE d.department_name = 'Marketing';
 
 WITH pedidos_por_cliente AS (SELECT customer_id, COUNT(*) AS total_pedidos, SUM(total_amount) AS valor_total FROM orders GROUP BY customer_id) SELECT c.first_name, c.last_name, p.total_pedidos, ROUND(p.valor_total / p.total_pedidos, 2) AS ticket_medio FROM pedidos_por_cliente p JOIN customers c ON c.customer_id = p.customer_id ORDER BY ticket_medio DESC;
+
+WITH vendas_por_cidade AS (SELECT c.city, SUM(o.total_amount) AS total_vendas FROM customers c JOIN orders o ON c.customer_id = o.customer_id GROUP BY c.city) SELECT * FROM vendas_por_cidade WHERE total_vendas > 1000 ORDER BY total_vendas DESC;
+
+WITH vendas_produto AS (SELECT p.product_name, SUM(oi.quantity) AS total_vendido FROM products p JOIN order _items oi ON p.product_id = oi.product_id GROUP BY p.product_name) SELECT * FROM vendas_produto ORDER BY total_vendido DESC LIMIT 10;
+
+WITH RECURSIVE numeros AS (SELECT 1 AS n UNION ALL SELECT n + 1 FROM numeros WHERE n < 10) SELECT * FROM numeros;
