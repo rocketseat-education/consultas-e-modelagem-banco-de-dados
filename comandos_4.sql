727cf1bfb6e7c40d73b7edb4d85078d57a00ab28
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2025-06-27 16:02:57 -0300

    Otimizando consultas complexas - Reescrita de consultas e refatoração

--- comandos_4.sql
+++ comandos_4.sql
@@ -43,3 +43,9 @@ EXPLAIN ANALYZE SELECT * FROM products WHERE price > 300;
 SET enable_seqscan = on;
 
 EXPLAIN ANALYZE SELECT o.order_id, c.first_name, c.last_name FROM orders o JOIN customers c ON o.customer_id = c.customer_id WHERE o.status = 'SHIPPED';
+
+SELECT o.order_id, (SELECT COUNT (*) FROM order_items oi WHERE oi.order_id = o.order_id) AS itens FROM orders o WHERE o.status = 'DELIVERED';
+
+SELECT o.order_id, COUNT (oi.order_id) AS itens FROM orders o LEFT JOIN order_items oi ON o.order_id = oi.order_id WHERE o.status = 'DELIVERED' GROUP BY o.order_id;
+
+WITH soma_itens AS (SELECT order_id, SUM(quantity * unit_price) AS subtotal FROM order_items GROUP BY order_id) SELECT o.order_id, o.total_amount, si.subtotal FROM orders o JOIN soma_itens si ON si.order_id = o.order_id WHERE o.status = 'DELIVERED';
